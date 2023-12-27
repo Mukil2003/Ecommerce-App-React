5fbc53ce27d58be13318a01836fb8ca54671862c
JubieSparkle <sparkle15803@gmail.com>
2023-12-27 12:06:34 +0530

    Started the project

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,8 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
+import logo from "../Assets/logo.png";
+import cart_logo from "../Assets/cart_icon.png";
 
 function Navbar() {
-  return <div></div>;
+  let [menu, setMenu] = useState("shop");
+
+  return (
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src={logo} alt="logo" />
+        <p>Shopify</p>
+      </div>
+
+      <ul className="nav-menu">
+        <li
+          onClick={() => {
+            setMenu("shop");
+          }}
+        >
+          Shop{menu === "shop" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("men");
+          }}
+        >
+          Men{menu === "men" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("women");
+          }}
+        >
+          Women {menu === "women" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("kids");
+          }}
+        >
+          Kids{menu === "kids" ? <hr /> : <></>}
+        </li>
+      </ul>
+
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart_logo} alt="cart" />
+        <div className="nav-cart-count">0</div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
